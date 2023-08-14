8ce86eb84f054dde8b589f1edc9378b3b1b18f3c
Ayush Thakur <116510022+Ayush-Thakur-geek@users.noreply.github.com>
2023-08-14 22:43:10 +0530

    Add files via upload

---
+++ templateLiterals.js
@@ -0,0 +1,10 @@
+let boy1 = "Ayush";
+let boy2 = "xyz";
+let sentence = `${boy1} is a friend of ${boy2}`;
+console.log(sentence);
+
+//Escape sequence characters
+
+let a = "hello \t ft";
+
+console.log(a);
