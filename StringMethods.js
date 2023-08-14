5a75c51b71b6f7a009897dd3b6cb8e54c86c59d5
Ayush Thakur <116510022+Ayush-Thakur-geek@users.noreply.github.com>
2023-08-15 00:00:25 +0530

    Add files via upload

---
+++ StringMethods.js
@@ -0,0 +1,11 @@
+let name = "Ayush";
+
+console.log(name.toUpperCase());
+console.log(name.toLowerCase());
+console.log(name.slice(2, 4));
+console.log(name.slice(2));
+console.log(name.replace("Ay", "R"));
+
+let b = "xyz";
+let c = name.concat(" is a friend of ", b);
+console.log(c);
